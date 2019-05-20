248131d296b0cb3686f94bce64b6a4cdff7b710a
arie yanuartaba <arieyanuartaba@yahoo.co.id>
2019-05-20 17:07:44 +0700

    CRUD Question with question Resource done

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Model\Question;
+use App\HTTP\Resources\QuestionResource;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -14,19 +15,10 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+   
     /**
      * Store a newly created resource in storage.
      *
@@ -35,7 +27,9 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // auth()->user()->question()->create($request->all());
+        Question::create($request->all());
+        return response('created', 201);
     }
 
     /**
@@ -46,19 +40,10 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return  new QuestionResource($question);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
+  
 
     /**
      * Update the specified resource in storage.
@@ -69,7 +54,9 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        
+        $question->update($request->all());
+        return response('updated', 202);
     }
 
     /**
@@ -80,6 +67,8 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return response(null, 204);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,6 +9,15 @@ use App\Category;
 
 class Question extends Model
 {
+    protected $guarded = [];
+    //protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+
+    public function getRouteKeyName(){
+        
+        return 'slug';
+
+    }
+
     public function user(){
 
         return $this->belongsTo(User::class);
@@ -21,5 +30,10 @@ class Question extends Model
 
     public function category() {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute() {
+
+        return asset("api/question/$this->slug");
     }
 }
