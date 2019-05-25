8f1ffc3916eb213e64aba2f3378790b6e84d98a5
arie yanuartaba <arieyanuartaba@yahoo.co.id>
2019-05-25 11:40:29 +0700

    Component vue, Vuetify package and Vue-router package done

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-nav></app-nav>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import AppNav from './AppNav';
+import AppFooter from './AppFooter';
+
+export default {
+    components: {
+        AppNav,
+        AppFooter
+    }
+}
+</script>
+
+<style>
+    
+</style>
